9e269bf2c43da9dbb9acf3541cbded0e81f7b32e
analuisadev <analuisavidal89@gmail.com>
2024-05-07 08:38:47 -0300

    Tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TiposVariaveis {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Tipos Primitivos
+            // Lógicos
+                boolean verdadeiroOuFalso = true;
+            // Texto
+                String Meunome = "Ana Luisa";
+            // Inteiros
+                byte idade = 21;
+                short ano = 2024;
+                int cep = 43841970;
+                long cpf = 84948306517L;
+                float pi = 3.14F;
+                double salario = 2800;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // Manipulação de variáveis do tipo short e int e análise de risco
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
+            System.out.println(numeroCurto2);
+
+            // Manipulação de variável do tipo int
+            int numero1 = 5;
+            numero1 = 10;
+
+            System.out.println(numero1);
+
+            // Declaração de constantes
+            final double VALOR_DE_PI = 3.14;
+            System.out.println(VALOR_DE_PI);
         }
-    }
 }
